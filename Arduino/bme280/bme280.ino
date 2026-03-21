8c0d91580e63c604f2d78f1328f166247b68184f
Alexandra Krause <alexandra.krause2@gmail.com>
2026-03-21 14:18:31 -0700

    Enhance code documentation with German comments
    Added comments in German for clarity and understanding of the code.

--- Arduino/bme280/bme280.ino
+++ Arduino/bme280/bme280.ino
@@ -16,10 +16,34 @@
   See the LICENSE file for details.
  ***************************************************************************/
 
+/* ## personal changes by sycramore denoted by ##
+*/
+
+/*
+## general : Datei ist deutlich weniger ausfuehrlich kommentiert als die Bosch-Library
+  */
+
+// ## Seengreat benutzt die Adafruit-Library. Potentiell gute Wahl, Adafruit bietet guten ausfuehrlichen Kundensupport fuer Anfaengersoftware und
+// ## Projekte und holt meines Wissens nach die somit entstehenden Kosten ueber den deutlich hoeheren Preis der verkauften Hardware wieder rein.
+// ## Heisst aber auch, dass eine Adafruit-Library wahrscheinlich gut getestet ist und sauber geschrieben/dokumentiert?
+
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+
+
+// ## Seengreat bietet anscheinend Support fuer die beiden gaengigen Protokolle I2C und SPI.
+// ## I2C - seriell (eine Datenleitung, Daten werden Zeichen fuer Zeichen gesendet - einfach, aber langsam)
+// ## I2C - meistens - ein Master, mehrere Slaves, also eine zentrale Kontrollstelle mit vielen empfangenden Empfaengern
+// ## I2C - benoetigt 2 Leitungen, SDA (Datenleitung, serial data) und SCL (Zeitsignal, seria clock)
+// ## Master gibt Taktrate vor
+// ## SPI - drei Leitungen, serielle Uebertragung, aber parallele Kommunikation moeglich durch die beiden Datenleitungen zwischen Master und Slave
+
+// ## SPI - 
+
+// ## Spannend, SPI scheint Standard zu sein, I2C als Zusatz ueber die externe Adafruit Library integriert
+// ## Warum? Ich haette gedacht, dass I2C als Protokoll deutlich einfacher sein sollte als SPI. Ist SPI vielseitiger und daher Standard?
 
 //If USEIIC is 1 means use I2C interface, If it is 0,use SPI interface
 #define USE_IIC 1
@@ -27,6 +51,8 @@
 #if USE_IIC
 Adafruit_BME280 bme; // I2C
 #else
+
+// ## Definition der I/O-Ports fuer Kommunikation, die Zahlen sind typischerweise die Portnummern, die im Datenblatt des Arduino stehen
 #define BME_SCK 13
 #define BME_MISO 12
 #define BME_MOSI 11
@@ -34,12 +60,18 @@ Adafruit_BME280 bme; // I2C
 Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
 #endif
 
+
+// ## Die Zahl sieht bekannt aus. HPA koennten Hektopascal sein. Ich kenne die 1013 als Druck aus Standardbedingungen und Normalbedingungen in der physikalischen
+// ## Chemie. Sea level, also Meeresspiegel ist bei Hoehenmessungen der Nullwert. Da die Hoehe des Luftdrucks abhaengig ist von der Hoehe relativ zum Meeresspiegel,
+// ## ergibt es Sinn, diesen Nullwert einmal festzulegen fuer Messungen von Luftdruck. Die Temperaturabhaengigkeit koennte man dann ueber den Sensor mit integrieren,
+// ## bzw der Sensor misst ja die Temperatur auch mit. Die Hoehenabhaengigkeit des Luftdrucks koennte man aber extra in eventuelle Rechnungen bzw
+// ## Ausgaben mit einbeziehen
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 unsigned long delayTime;
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(115200); //## 115200 Baud ist die hoechste serielle Uebertragungsrate bei Arduino
     while(!Serial);    // time to get serial running
     Serial.println(F("BME280 test"));
 
@@ -48,7 +80,7 @@ void setup() {
     // default settings
     status = bme.begin();  
     // You can also pass in a Wire library object like &Wire2
-    //status = bme.begin(0x76, &Wire2);
+    //status = bme.begin(0x76, &Wire2); //## wieso die 0x76? Was macht der Wert?
     if (!status) {
         Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
         Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
@@ -71,7 +103,8 @@ void loop() {
     delay(delayTime);
 }
 
-
+// ## gut, Seengreat ruft also nur die Adafruit-Library auf als Demobeispiel und demonstriert ein paar essentielle Funktionen der Library
+// ## Wenn ich mehr wissen will, sollte ich also die Adafruit Library sezieren, falls diese Open Source ist.
 void printValues() {
     Serial.print("Temperature = ");
     Serial.print(bme.readTemperature());
